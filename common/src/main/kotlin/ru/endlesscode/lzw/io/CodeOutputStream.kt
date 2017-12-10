a1ad2402a9308102e898dba2c460bc65e12f16dc
OsipXD <osip.fatkullin@gmail.com>
2017-12-10 15:47:24 +0300

    Refactored and commented CodeOutputStream

--- common/src/main/kotlin/ru/endlesscode/lzw/io/CodeOutputStream.kt
+++ common/src/main/kotlin/ru/endlesscode/lzw/io/CodeOutputStream.kt
@@ -27,49 +27,79 @@ package ru.endlesscode.lzw.io
 
 import ru.endlesscode.lzw.util.Bytes
 
-
+/**
+ * Stream that can write out parts more than one byte.
+ *
+ * @param stream Target stream
+ * @param codeLength Determines part size
+ */
 class CodeOutputStream(
         private val stream: OutputStream,
         private val codeLength: Int
 ) {
 
+    /**
+     * Mask that used to fit code to given [codeLength].
+     */
     private val mask = Bytes.mask(codeLength)
-    private val bufSize = Bytes.BITS_IN_INT
 
-    private var buf = 0
-    private var bufUsedBits = 0
+    /**
+     * Size of [buffer].
+     */
+    private val bufferSize = Bytes.BITS_IN_INT
 
+    /**
+     * Buffer to store part of code that can't fit in byte.
+     * Used Int. It can store maximum 64 bits.
+     * @see [bufferSize]
+     */
+    private var buffer = 0
+
+    /**
+     * Used bits of [buffer]
+     */
+    private var usedBits = 0
+
+    /**
+     * Validates input params
+     */
     init {
-        if (codeLength > bufSize) {
+        if (codeLength > bufferSize) {
             throw IllegalArgumentException("Code length $codeLength is more than buffer size.")
         }
     }
 
     fun write(code: Int) {
-        val bufferedCode = (code and mask) shl (bufUsedBits)
-        buf = buf or bufferedCode
-        bufUsedBits += codeLength
+        val bufferedCode = (code and mask) shl (usedBits)
+        buffer = buffer or bufferedCode
+        usedBits += codeLength
 
         writeBuffer()
     }
 
+    /**
+     * Write full bytes from [buffer] to [stream].
+     */
     private fun writeBuffer() {
-        while (bufUsedBits >= Bytes.BITS_IN_BYTE) {
+        while (usedBits >= Bytes.BITS_IN_BYTE) {
             writeNextByte()
-            buf = buf ushr Bytes.BITS_IN_BYTE
-            bufUsedBits -= Bytes.BITS_IN_BYTE
+            buffer = buffer ushr Bytes.BITS_IN_BYTE
+            usedBits -= Bytes.BITS_IN_BYTE
         }
     }
 
     fun flush() {
-        if (buf != 0) {
+        if (buffer != 0) {
             writeNextByte()
         }
 
         stream.flush()
     }
 
+    /**
+     * Write next byte from [buffer] to [stream].
+     */
     private fun writeNextByte() {
-        stream.write(buf and Bytes.BYTE_MASK)
+        stream.write(buffer and Bytes.BYTE_MASK)
     }
 }
