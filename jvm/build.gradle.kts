9043b552fd9c18e738205350ad00213d8b3a00ab
OsipXD <osip.fatkullin@gmail.com>
2017-12-11 19:38:15 +0300

    Add fat jar building

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,7 +1,28 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    application
     id("kotlin-platform-jvm")
+    id("com.github.johnrengelman.shadow") version "2.0.1"
+}
+
+application {
+    mainClassName = "ru.endlesscode.lzw.CeymUtilKt"
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes.apply {
+            put("Implementation-Title", "CEYM util")
+            put("Implementation-Version", version)
+            put("Main-Class", application.mainClassName)
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 dependencies {
@@ -10,8 +31,4 @@ dependencies {
     implementation("info.picocli:picocli:2.1.0")
     testCompile(kotlin("test"))
     testCompile(kotlin("test-junit"))
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
